5a7d7daac219c74d78472b45fcfa9f13abdd5441
Jorge Rivas <eduardorivas140@gmail.com>
2020-10-26 21:19:18 -0600

    primer commit

---
+++ app/src/main/java/com/example/prodes/Grado.java
@@ -0,0 +1,14 @@
+package com.example.prodes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Grado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grado);
+    }
+}

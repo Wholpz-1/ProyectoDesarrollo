7014683ca8aae22b7fd12d0120a144a2be7cf2ed
Jorge Rivas <eduardorivas140@gmail.com>
2020-10-26 22:42:52 -0600

    comienzo violento

--- app/src/main/java/com/example/prodes/MainActivity.java
+++ app/src/main/java/com/example/prodes/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.prodes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+        public void onClick (View view){
+
+            Intent miIntent = null;
+
+            switch (view.getId()) {
+                case R.id.img_alumno:
+                    miIntent = new Intent(MainActivity.this, Alumno.class);
+                    break;
+                case R.id.Line_grado:
+                    miIntent = new Intent(MainActivity.this, Grado.class);
+                    break;
+                case R.id.Line_ins:
+                    miIntent = new Intent(MainActivity.this, Inscripcion.class);
+                    break;
+
+
+            }
+            startActivity(miIntent);
+        }
+
+
+    }
